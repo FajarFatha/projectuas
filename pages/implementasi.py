f427ed41dbfeeccdb33eb6e92baab2f60b9eb7cd
FajarFatha <fajarfathar@gmail.com>
2022-11-16 23:05:47 +0700

    memperbaiki file procfile

--- pages/implementasi.py
+++ pages/implementasi.py
@@ -3,7 +3,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-from sklearn.ensemble import RandomForestClassifier, StackingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import StackingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
